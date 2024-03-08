52c2557bd0831cb43a0a9d8cf3f991ed1745bfce
Rizz-33 <risiniamarathunga@gmail.com>
2024-03-08 19:36:06 +0530

    modify: main => light mode

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:whispr/pages/login.dart';
+import 'package:whispr/themes/light.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,6 +14,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: LoginPage(),
+      theme: lightMode,
     );
   }
 }
