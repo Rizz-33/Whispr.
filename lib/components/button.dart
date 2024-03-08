f27a536f74c281998bacf0d746b7980280e470ff
Rizz-33 <risiniamarathunga@gmail.com>
2024-03-08 22:21:33 +0530

    modify: ontap function

--- lib/components/button.dart
+++ lib/components/button.dart
@@ -1,21 +1,25 @@
 import 'package:flutter/material.dart';
 
 class Button extends StatelessWidget {
+  final void Function()? onTap;
   final String text;
 
-  const Button({super.key, required this.text});
+  const Button({super.key, required this.text, this.onTap});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        color: Theme.of(context).colorScheme.primary,
-        borderRadius: BorderRadius.circular(8)
-      ),
-      padding: EdgeInsets.all(16),
-      margin: EdgeInsets.symmetric(horizontal: 25),
-      child: Center(
-        child: Text(text),
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        decoration: BoxDecoration(
+          color: Theme.of(context).colorScheme.primary,
+          borderRadius: BorderRadius.circular(8)
+        ),
+        padding: EdgeInsets.all(16),
+        margin: EdgeInsets.symmetric(horizontal: 25),
+        child: Center(
+          child: Text(text),
+        ),
       ),
     );
   }
