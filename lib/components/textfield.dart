1ce0357a331cd1caefabed032393b15c080b6a72
Rizz-33 <risiniamarathunga@gmail.com>
2024-03-08 21:53:05 +0530

    feat: obsecure text

--- lib/components/textfield.dart
+++ lib/components/textfield.dart
@@ -2,14 +2,16 @@ import 'package:flutter/material.dart';
 
 class CustomTextField extends StatelessWidget {
   final String hintText;
+  final bool obscureText;
 
-  const CustomTextField({Key? key, required this.hintText}) : super(key: key);
+  const CustomTextField({Key? key, required this.hintText, required this.obscureText}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 25),
       child: TextField(
+        obscureText: obscureText,
         decoration: InputDecoration(
           enabledBorder: OutlineInputBorder(
             borderSide: BorderSide(color: Theme.of(context).colorScheme.secondary),
