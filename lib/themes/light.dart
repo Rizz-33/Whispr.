979afdea3f08f7e2c64f52c285970a6209c9137d
Rizz-33 <risiniamarathunga@gmail.com>
2024-03-08 19:33:00 +0530

    modify: theme ight

--- lib/themes/light.dart
+++ lib/themes/light.dart
@@ -2,9 +2,9 @@ import 'package:flutter/material.dart';
 
 ThemeData lightMode = ThemeData(
   colorScheme: ColorScheme.light(
-    background: Colors.grey.shade300,
-    primary: Colors.grey.shade500,
-    secondary: Colors.grey.shade200,
+    background: Color.fromARGB(255, 245, 245, 245),
+    primary: Color.fromARGB(255, 0, 47, 167),
+    secondary: Color.fromARGB(255, 188, 206, 255),
     tertiary: Colors.white,
     inversePrimary: Colors.grey.shade900,
   )
