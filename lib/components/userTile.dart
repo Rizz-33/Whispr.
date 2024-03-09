c6dd1c67e0d10b797ffb59041994a3cf6d8b50c2
Rizz-33 <risiniamarathunga@gmail.com>
2024-03-09 20:12:01 +0530

    style: padding

--- lib/components/userTile.dart
+++ lib/components/userTile.dart
@@ -14,6 +14,7 @@ class UserTile extends StatelessWidget {
           color: Theme.of(context).colorScheme.secondary,
           borderRadius: BorderRadius.circular(12),
         ),
+        margin: const EdgeInsets.symmetric(vertical: 5, horizontal: 25),
         child: Row(
           children: [
             //icon
