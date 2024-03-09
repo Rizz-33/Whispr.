3e323c695c55638809db0b5d38dc6ba46e4fb886
Rizz-33 <risiniamarathunga@gmail.com>
2024-03-09 09:30:22 +0530

    feat: scrollable login

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -10,51 +10,73 @@ class LoginPage extends StatelessWidget {
   LoginPage({super.key});
 
   //login method
-  void login(){
-
-  }
+  void login() {}
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Theme.of(context).colorScheme.background,
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            //logo
-            Image.asset('lib/images/logo.png', width: 300,),
-        
-            //welcome back message
-            Text('Welcome back, Thrilled to have you here again.', style: TextStyle(fontSize: 16, color: Colors.grey[700]),),
+      body: SingleChildScrollView(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              //logo
+              Image.asset(
+                'lib/images/logo.png',
+                width: 300,
+              ),
 
-            const SizedBox(height: 20,),
-        
-            //email
-            CustomTextField(hintText: 'Email', obscureText: false, controller: _emailController,),
+              //welcome back message
+              Text(
+                'Welcome back, Thrilled to have you here again.',
+                style: TextStyle(fontSize: 16, color: Colors.grey[700]),
+              ),
 
-            const SizedBox(height: 16,),
-        
-            //password
-            CustomTextField(hintText: 'Password', obscureText: true, controller: _passwordController,),
+              const SizedBox(height: 20,),
 
-            const SizedBox(height: 20,),
-        
-            //login button
-            Button(text: 'Login', onTap: () {  },),
+              //email
+              CustomTextField(
+                hintText: 'Email',
+                obscureText: false,
+                controller: _emailController,
+              ),
 
-            const SizedBox(height: 20,),
+              const SizedBox(height: 16,),
 
-        
-            //register now
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text('Not a member? ', style: TextStyle(color: Colors.grey[700]),),
-                Text('Register now', style: TextStyle(fontWeight: FontWeight.bold),),
-              ],
-            )
-          ],
+              //password
+              CustomTextField(
+                hintText: 'Password',
+                obscureText: true,
+                controller: _passwordController,
+              ),
+
+              const SizedBox(height: 20,),
+
+              //login button
+              Button(
+                text: 'Login',
+                onTap: () {},
+              ),
+
+              const SizedBox(height: 20,),
+
+              //register now
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    'Not a member? ',
+                    style: TextStyle(color: Colors.grey[700]),
+                  ),
+                  Text(
+                    'Register now',
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                ],
+              )
+            ],
+          ),
         ),
       ),
     );
