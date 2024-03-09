5634b2c3f178be85615a350fc553e1f791f5d279
Rizz-33 <risiniamarathunga@gmail.com>
2024-03-09 12:17:10 +0530

    style: drawer list

--- lib/components/drawer.dart
+++ lib/components/drawer.dart
@@ -8,19 +8,48 @@ class CustomDrawer extends StatelessWidget {
     return Drawer(
       backgroundColor: Theme.of(context).colorScheme.background,
       child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
           //logo
-          DrawerHeader(child:
-            Image.asset(
-              'lib/images/logo.png',
-              width: 180,
-            ))
-
-          //home list title
-
-          //settings list title
+          Column(
+            children: [
+              DrawerHeader(child:
+                Image.asset(
+                  'lib/images/logo.png',
+                  width: 240,
+                )),
+              
+              //home list title
+              Padding(
+                padding: const EdgeInsets.only(left: 25.0),
+                child: ListTile(
+                  title: Text("H O M E", style: TextStyle(color: Colors.grey[700]),),
+                  leading: Icon(Icons.home, color: Colors.grey[600],),
+                  onTap: (){},
+                ),
+              ),
+              
+              //settings list title
+              Padding(
+                padding: const EdgeInsets.only(left: 25.0),
+                child: ListTile(
+                  title: Text("S E T T I N G S", style: TextStyle(color: Colors.grey[700]),),
+                  leading: Icon(Icons.settings, color: Colors.grey[600],),
+                  onTap: (){},
+                ),
+              ),
+            ],
+          ),
 
           //logout list tile
+          Padding(
+            padding: const EdgeInsets.only(left: 25.0, bottom: 25),
+            child: ListTile(
+              title: Text("L O G O U T", style: TextStyle(color: Colors.grey[700]),),
+              leading: Icon(Icons.logout, color: Colors.grey[600],),
+              onTap: (){},
+            ),
+          )
         ],
       ),
     );
