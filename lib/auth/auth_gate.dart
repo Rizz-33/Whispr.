062b8543d063f39c83f22cc8acf3fb7c50977585
Rizz-33 <risiniamarathunga@gmail.com>
2024-03-09 11:17:15 +0530

    feat: authgate file

---
+++ lib/auth/auth_gate.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AuthGate extends StatelessWidget {
+  const AuthGate({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
