06df0a76bc1a154ddbb9b59fe3bfcc82dc524105
Rizz-33 <risiniamarathunga@gmail.com>
2024-03-09 11:07:55 +0530

    feat: sign in method [auth]

--- lib/auth/auth_service.dart
+++ lib/auth/auth_service.dart
@@ -1,9 +1,18 @@
+import 'package:firebase_auth/firebase_auth.dart';
+
 class AuthService {
   //instances of auth
-
+  final FirebaseAuth _auth = FirebaseAuth.instance;
 
   //sign in
-
+  Future<UserCredential> signInWithEmailPassword(String email, password) async {
+    try {
+      UserCredential userCredential = await _auth.signInWithEmailAndPassword(email: email, password: password);
+      return userCredential;
+    } on FirebaseAuthException catch (e) {
+      throw Exception(e.code);
+    }
+  }
 
   //sign up
 
