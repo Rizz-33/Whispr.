72bd14b502525625f2a27b8e91630c63e623bfa1
Rizz-33 <risiniamarathunga@gmail.com>
2024-03-09 18:39:17 +0530

    feat: get instance of firestore

--- lib/services/chat/chat_service.dart
+++ lib/services/chat/chat_service.dart
@@ -1,6 +1,9 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
 class ChatService {
 
   //get instance of firestore
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
 
 
   //get user stream
