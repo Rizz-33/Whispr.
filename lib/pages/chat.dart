426be5ae074821c519369024cef0c2bd7c521623
Rizz-33 <risiniamarathunga@gmail.com>
2024-03-09 21:18:22 +0530

    feat: controller message chat

--- lib/pages/chat.dart
+++ lib/pages/chat.dart
@@ -3,7 +3,10 @@ import 'package:flutter/material.dart';
 class ChatPage extends StatelessWidget {
   final String receiverEmail;
 
-  const ChatPage({super.key, required this.receiverEmail});
+  ChatPage({super.key, required this.receiverEmail});
+
+  //text controller
+  final TextEditingController _messageController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
