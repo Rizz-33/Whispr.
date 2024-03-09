b80a20685f43c0dc9d7487160b2df0c992fb88da
Rizz-33 <risiniamarathunga@gmail.com>
2024-03-09 19:30:06 +0530

    feat: sigin save user

--- lib/services/auth/auth_service.dart
+++ lib/services/auth/auth_service.dart
@@ -10,6 +10,15 @@ class AuthService {
   Future<UserCredential> signInWithEmailPassword(String email, password) async {
     try {
       UserCredential userCredential = await _auth.signInWithEmailAndPassword(email: email, password: password);
+
+      // save user if it doesnt already exist
+      _firestore.collection("Users").doc(userCredential.user!.uid).set(
+        {
+          'uid' : userCredential.user!.uid,
+          'email' : email,
+        }
+      );
+
       return userCredential;
     } on FirebaseAuthException catch (e) {
       throw Exception(e.code);
