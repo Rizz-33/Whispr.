a034b462810296d2d83aee5bfb085affd1ea40b3
Rizz-33 <risiniamarathunga@gmail.com>
2024-03-09 10:15:59 +0530

    feat: login or register [auth]

---
+++ lib/auth/login_or_register.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class LoginOrRegister extends StatefulWidget {
+  const LoginOrRegister({super.key});
+
+  @override
+  State<LoginOrRegister> createState() => _MyWidgetState();
+}
+
+class _MyWidgetState extends State<LoginOrRegister> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
