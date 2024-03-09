0dbc3504a43163908819f7518e918f2db000f52e
Rizz-33 <risiniamarathunga@gmail.com>
2024-03-09 11:59:03 +0530

    feat: drawer

---
+++ lib/components/drawer.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class CustomDrawer extends StatelessWidget {
+  const CustomDrawer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      backgroundColor: Theme.of(context).colorScheme.background,
+      child: Column(
+        children: [
+          //logo
+
+          //home list title
+
+          //settings list title
+
+          //logout list tile
+        ],
+      ),
+    );
+  }
+}
