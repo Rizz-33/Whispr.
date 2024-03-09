499f8f16da95aa01e0068b7695b51b2733efe334
Rizz-33 <risiniamarathunga@gmail.com>
2024-03-09 11:26:59 +0530

    feat: sign in suc..

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -3,11 +3,19 @@ import 'package:flutter/material.dart';
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
+  void logout(){
+    
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Home'),
+        actions: [
+          //logout button
+          IconButton(onPressed: logout, icon: Icon(Icons.logout)),
+        ],
       ),
     );
   }
