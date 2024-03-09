0d71a6825971c0256713afbcfea771d20c1ff039
Rizz-33 <risiniamarathunga@gmail.com>
2024-03-09 09:44:29 +0530

    modify: register

--- lib/pages/register.dart
+++ lib/pages/register.dart
@@ -1,10 +1,82 @@
 import 'package:flutter/material.dart';
+import 'package:whispr/components/button.dart';
+import 'package:whispr/components/textfield.dart';
 
 class Register extends StatelessWidget {
-  const Register({super.key});
+  //email and password controllers
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+
+
+  Register({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.background,
+      body: SingleChildScrollView(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              //logo
+              Image.asset(
+                'lib/images/logo.png',
+                width: 300,
+              ),
+
+              //welcome back message
+              Text(
+                'Welcome back, Thrilled to have you here again.',
+                style: TextStyle(fontSize: 16, color: Colors.grey[700]),
+              ),
+
+              const SizedBox(height: 20,),
+
+              //email
+              CustomTextField(
+                hintText: 'Email',
+                obscureText: false,
+                controller: _emailController,
+              ),
+
+              const SizedBox(height: 16,),
+
+              //password
+              CustomTextField(
+                hintText: 'Password',
+                obscureText: true,
+                controller: _passwordController,
+              ),
+
+              const SizedBox(height: 20,),
+
+              //login button
+              Button(
+                text: 'Login',
+                onTap: () {},
+              ),
+
+              const SizedBox(height: 20,),
+
+              //register now
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    'Not a member? ',
+                    style: TextStyle(color: Colors.grey[700]),
+                  ),
+                  Text(
+                    'Register now',
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                ],
+              )
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
