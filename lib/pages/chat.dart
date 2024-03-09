45aff25be832f7adebf4f2bf2daecb5c1e79b1cf
Rizz-33 <risiniamarathunga@gmail.com>
2024-03-09 21:19:55 +0530

    feat: chat and auth services

--- lib/pages/chat.dart
+++ lib/pages/chat.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:whispr/services/auth/auth_service.dart';
+import 'package:whispr/services/chat/chat_service.dart';
 
 class ChatPage extends StatelessWidget {
   final String receiverEmail;
@@ -7,6 +9,10 @@ class ChatPage extends StatelessWidget {
 
   //text controller
   final TextEditingController _messageController = TextEditingController();
+
+  //chat and auth service
+  final ChatService _chatService = ChatService();
+  final AuthService _authService = AuthService();
 
   @override
   Widget build(BuildContext context) {
