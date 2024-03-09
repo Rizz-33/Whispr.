d74154a12b544a110e5882ddfdc8fc59c6f7f2b4
Rizz-33 <risiniamarathunga@gmail.com>
2024-03-09 19:07:32 +0530

    feat: user tile file

---
+++ lib/components/userTile.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class UserTile extends StatelessWidget {
+  const UserTile({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
