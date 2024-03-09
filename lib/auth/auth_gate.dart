79569718a899a4e4fe4eb60f657a20b5ff049cd9
Rizz-33 <risiniamarathunga@gmail.com>
2024-03-09 11:21:06 +0530

    feat: validation [login or not]

--- lib/auth/auth_gate.dart
+++ lib/auth/auth_gate.dart
@@ -1,10 +1,27 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:whispr/auth/login_or_register.dart';
 
 class AuthGate extends StatelessWidget {
   const AuthGate({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: StreamBuilder(
+        stream: FirebaseAuth.instance.authStateChanges(),
+        builder: (context, snapshot) {
+          //user logged in
+          if (snapshot.hasData) {
+            return const HomePage();
+          }
+
+          //user is not logged in
+          else {
+            return const LoginOrRegister();
+          }
+        },
+      ),
+    );
   }
 }
