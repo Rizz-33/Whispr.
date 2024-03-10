a15a2fd985d399098b4bbae34b4fb1c2bd326606
Rizz-33 <risiniamarathunga@gmail.com>
2024-03-10 12:25:54 +0530

    modify: container style

--- lib/pages/settings.dart
+++ lib/pages/settings.dart
@@ -14,17 +14,25 @@ class SettingsPage extends StatelessWidget {
         foregroundColor: Colors.grey,
         elevation: 0,
       ),
-      body: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          //dark mode
-          Text("Dark Mode"),
-
-          //switch
-          CupertinoSwitch(value: true, onChanged: (value){
-
-          })
-        ],
+      body: Container(
+        decoration: BoxDecoration(
+          color: Theme.of(context).colorScheme.secondary,
+          borderRadius: BorderRadius.circular(8),
+        ),
+        margin: EdgeInsets.all(25),
+        padding: EdgeInsets.all(16),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            //dark mode
+            Text("Dark Mode"),
+        
+            //switch
+            CupertinoSwitch(value: true, onChanged: (value){
+        
+            })
+          ],
+        ),
       ),
     );
   }
