81f9facc0e4e60274279c251f66643fbdebe59de
Rizz-33 <risiniamarathunga@gmail.com>
2024-03-10 13:09:08 +0530

    modfiy: colors dark

--- lib/themes/dark.dart
+++ lib/themes/dark.dart
@@ -4,7 +4,7 @@ ThemeData darkMode = ThemeData(
   colorScheme: ColorScheme.dark(
     background: Color.fromARGB(255, 28, 28, 28),
     primary: Color.fromARGB(255, 87, 134, 255),
-    secondary: Color.fromARGB(255, 0, 17, 66),
+    secondary: Color.fromARGB(255, 16, 37, 101),
     tertiary: Colors.black,
     inversePrimary: Colors.grey.shade300,
   )
