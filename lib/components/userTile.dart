01201b247578bf1435f1b9cc54ba3e53b1787128
Rizz-33 <risiniamarathunga@gmail.com>
2024-03-10 14:53:26 +0530

    modify: tile

--- lib/components/userTile.dart
+++ lib/components/userTile.dart
@@ -12,9 +12,9 @@ class UserTile extends StatelessWidget {
       child: Container(
         decoration: BoxDecoration(
           color: Theme.of(context).colorScheme.secondary,
-          borderRadius: BorderRadius.circular(12),
+          borderRadius: BorderRadius.circular(24),
         ),
-        margin: const EdgeInsets.symmetric(vertical: 5, horizontal: 25),
+        margin: const EdgeInsets.symmetric(vertical: 8, horizontal: 25),
         padding: EdgeInsets.all(20),
         child: Row(
           children: [
