df128335d36dc778ad9e349fd1ab807ec934bbf4
Rizz-33 <risiniamarathunga@gmail.com>
2024-03-10 12:00:49 +0530

    modify: get n set

--- lib/themes/thteme_provider.dart
+++ lib/themes/thteme_provider.dart
@@ -1,5 +1,11 @@
-import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:whispr/themes/dark.dart';
+import 'package:whispr/themes/light.dart';
 
 class ThemeProvider extends ChangeNotifier {
-  
+  ThemeData _themeData = lightMode;
+
+  ThemeData get themeData => _themeData;
+
+  bool get isDarkMode => _themeData == darkMode;
 }
