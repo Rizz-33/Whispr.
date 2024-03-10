ca940a0a67337f638190adca15af2254690a023d
Rizz-33 <risiniamarathunga@gmail.com>
2024-03-10 11:48:35 +0530

    style: settings backgrounf

--- lib/pages/settings.dart
+++ lib/pages/settings.dart
@@ -6,6 +6,7 @@ class SettingsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.background,
       appBar: AppBar(
         title: Text('Settings'),
         backgroundColor: Colors.transparent,
