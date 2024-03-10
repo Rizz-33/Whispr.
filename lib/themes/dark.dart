beaea93860a753861ff1a25686beefd60337b961
Rizz-33 <risiniamarathunga@gmail.com>
2024-03-10 11:50:39 +0530

    feat: dark theme file

---
+++ lib/themes/dark.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+ThemeData lightMode = ThemeData(
+  colorScheme: ColorScheme.light(
+    background: Color.fromARGB(255, 245, 245, 245),
+    primary: Color.fromARGB(255, 0, 47, 167),
+    secondary: Color.fromARGB(255, 188, 206, 255),
+    tertiary: Colors.white,
+    inversePrimary: Colors.grey.shade900,
+  )
+);
